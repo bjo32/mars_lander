d47dd4c9169f36b5ab59c66e099a9dbca5bec1be
bjo32 <bjo32@cam.ac.uk>
2026-06-22 16:28:31 +0100

    verlet integrator

--- spring_by_verlet.py
+++ spring_by_verlet.py
@@ -11,14 +11,17 @@ v = 1
 
 # simulation time, timestep and time
 t_max = 100
-dt = 0.1
+dt = 10
 t_array = np.arange(0, t_max, dt)
 
 # initialise empty lists to record trajectories
 x_list = []
 v_list = []
 
-# Euler integration
+# initialize previous position for Verlet method
+x_prev = x - v * dt
+
+# verlet integration
 for t in t_array:
 
     # append current state to trajectories
@@ -27,8 +30,10 @@ for t in t_array:
 
     # calculate new position and velocity
     a = -k * x / m
-    x = x + dt * v
-    v = v + dt * a
+    x_new = 2 * x - x_prev + dt**2 * a
+    v = (x_new - x) / dt
+    x_prev = x
+    x = x_new
 
 # convert trajectory lists into arrays, so they can be sliced (useful for Assignment 2)
 x_array = np.array(x_list)
@@ -36,9 +41,10 @@ v_array = np.array(v_list)
 
 
 # plot the position-time graph
-plt.figure(1)
-plt.clf()
+plt.close('all')
+plt.figure()
 plt.xlabel('time (s)')
+plt.ylabel('position / velocity')
 plt.grid()
 plt.plot(t_array, x_array, label='x (m)')
 plt.plot(t_array, v_array, label='v (m/s)')
