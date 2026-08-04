e164a1c0747682b914cba1dc9d0b12cb9bf2faf9
bjo32 <bjo32@cam.ac.uk>
2026-08-04 12:03:46 +0100

    verlet spring

--- lander/my-project/src/index.ts
+++
@@ -1,7 +0,0 @@
-// This file is the entry point of the application. It contains the main logic and exports any necessary functions or classes for the project.
-
-export const main = () => {
-    console.log("Hello, World!");
-};
-
-// Additional exports can be added here as needed.
